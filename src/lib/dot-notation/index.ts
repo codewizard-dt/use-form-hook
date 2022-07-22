f67ed8232fc205ffab6a9d40b8c2d59a7f04b7d8
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-07-22 05:36:39 -0500

    Recursive FieldGroups and FormData

--- src/lib/dot-notation/index.ts
+++ src/lib/dot-notation/index.ts
@@ -1,12 +1,28 @@
 import { at, set } from "lodash"
 
-export type KeyedData = { [key: string]: string }
-export type NestedData = { [key: string]: KeyedData | string }
+export type KeyedData = { [key: string]: KeyedData | string }
+export type FlatData = { [key: string]: string }
+// export type NestedData = { [key: string]: KeyedData | string }
 
-export function getDot(dotNotation: string, obj: NestedData): KeyedData | string | undefined {
+export function getDot(dotNotation: string, obj: KeyedData): KeyedData | string | undefined {
   return at(obj, dotNotation)[0]
 }
 
-export function setDot(dotNotation: string, value: string, obj: NestedData) {
+export function setDot(dotNotation: string, value: string, obj: KeyedData) {
   return set(obj, dotNotation.split('.'), value)
 }
+
+export function getFlatObj(obj: KeyedData): FlatData {
+  let flat: FlatData = {}
+  for (let [key, value] of Object.entries(obj)) {
+    if (typeof value === 'string') flat[key] = value
+    else {
+      let next = getFlatObj(value)
+      for (let [key2, val2] of Object.entries(next)) {
+        flat[`${key}.${key2}`] = val2
+      }
+    }
+  }
+  return flat
+}
+
